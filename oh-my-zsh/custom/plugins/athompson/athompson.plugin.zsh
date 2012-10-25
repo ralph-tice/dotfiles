73d1f47200f42daa879205ad76cf6d6b1d325b0a
Andrew Thompson <netengr2009@gmail.com>
2012-10-25 15:57:51 -0400

    Unaliasing some dangerous git commands

--- oh-my-zsh/custom/plugins/athompson/athompson.plugin.zsh
+++ oh-my-zsh/custom/plugins/athompson/athompson.plugin.zsh
@@ -9,6 +9,12 @@ export EDITOR='vim'
 # autocorrect is more annoying than helpful
 unsetopt correct_all
 
+# disabling some dragons
+unalias gp # git push
+unalias git-svn-dcommit-push # git and svn mix 
+unalias ggpush # git push current branch
+unalias ggpnp # git pull and push current branch
+
 # a few aliases I like
 alias gs='git status'
 alias gd='git diff'
